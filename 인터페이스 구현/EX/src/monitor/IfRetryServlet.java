7d85c99dfcd2de17847beefac1bc0b572dcb7c9d
1gyeolpark <marine3682@gmail.com>
2026-05-26 14:42:07 +0900

    V 0.0 박한결 - DAO 객체 선언 & 서블릿 초기화

--- 인터페이스 구현/EX/src/monitor/IfRetryServlet.java
+++ 인터페이스 구현/EX/src/monitor/IfRetryServlet.java
@@ -10,13 +10,20 @@ import javax.servlet.http.HttpServletResponse;
 
 import send.OutboxDAO;
 
-@WebServlet("/monitor/retry_")
+//----------------------------------------------------------------
+//실패한 발신 메시지(Outbox)의 상태를 '재시도(F→N)' 가능하도록 변경하고
+//모니터링 목록 페이지로 리다이렉트(Redirect)하는 MVC 패턴의 컨트롤러
+//----------------------------------------------------------------
+
+@WebServlet("/monitor/retry_")	// "/monitor/retry_" URL로 요청 보내면 이 서블릿이 실행되도록 매핑
 public class IfRetryServlet extends HttpServlet {
+	
+	// 데이터베이스 연동 처리할 OutboxDAO 객체 선언
+    private OutboxDAO outboxDAO; 
 
-    private OutboxDAO outboxDAO;
-
+    // 서블릿 초기화. 사용할 OutboxDAO 객체 미리 생성
     @Override
-    public void init() {
+    public void init() { // init(): 서블릿이 메모리에 로드될 때 최초 1회 실행되는 초기화 메서드
         outboxDAO = new OutboxDAO();
     }
 
